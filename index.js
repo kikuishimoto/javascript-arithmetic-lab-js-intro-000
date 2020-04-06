13ea9fb61c6c1a59291fcd122e1241cdc26301a9
kiku ishimoto <kikuish@gmail.com>
2020-04-06 07:37:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ function divide(){
 function add(a,b){
   return a + b
 }
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
